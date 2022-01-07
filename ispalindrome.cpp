05c396c6ee4c306578977266040add6e020aaf14
PARADOX-XU <65709863+PARADOX-XU@users.noreply.github.com>
2022-01-06 19:33:39 -0800

    Update ispalindrome.cpp

--- ispalindrome.cpp
+++ ispalindrome.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
 using namespace std;
-bool palPal(string x);
 
-bool isPalindrome(int x){
-   // int length = 0;
-    std::string str = std::to_string(x);
-    return palPal(str);
-
-   
+bool is_Palindrome(int text){
+    std::string str = std::to_string(text);
+    return palPal(str);  
 }
+
