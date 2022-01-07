89dbb37dbdefc0f927f5e1a10cb479a9958d35da
PARADOX-XU <65709863+PARADOX-XU@users.noreply.github.com>
2022-01-06 19:23:45 -0800

    Update addone.cpp

--- addone.cpp
+++ addone.cpp
@@ -5,19 +5,3 @@ using namespace std;
 void addOne (int *ptrNum){
     *ptrNum=*ptrNum + 1;
 }
-int main(){
-    int x = 11;
-    int *intPtr = &x;
-
-    cout << "Address of x " << intPtr << endl;
-    cout << "Dereference ptr "<< *intPtr << endl;
-    cout << "address of ptr " << &intPtr << endl;
-    
-    addOne(&x);
-    cout << "After addOne " << *intPtr << endl;
-    cout << "Address of x " << intPtr << endl;
-    cout << "Dereference ptr "<< *intPtr << endl;
-    cout << "address of ptr " << &intPtr << endl;
-    
-    return 0;
-}
