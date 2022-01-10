b225a88b38234a71c414aa4fbaaac49b76cf8b2f
PARADOX-XU <65709863+PARADOX-XU@users.noreply.github.com>
2022-01-09 21:15:46 -0800

    Update showticket.h

--- showticket.h
+++ showticket.h
@@ -1,15 +1,32 @@
 #include <string>
 using namespace std;
 
-      bool is_sold(){         
-            return sold;
-        }
+class ShowTicket {
+    private:
+        bool sold;
+    
+    protected:    
+        string row;
+        string seat;
+        
+    public:
+        bool is_sold();
 
-      void sell_seat(){ 
+        ShowTicket(std::string s, std::string r);
+        
+        void sell_seat();
+
+        string print_ticket();
+
+};
+      bool ShowTicket::is_sold(){         
+            return sold;
+        }     
+        void ShowTicket::sell_seat(){ 
             sold = true;
             }
 
-      string print_ticket(){
+        string ShowTicket::print_ticket(){
             if (sold)
                 return row + " "+ seat + " sold";
             else
