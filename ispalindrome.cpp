a53c242e9eb58dc9ff20cf3cf8935bb2676baca2
PARADOX-XU <65709863+PARADOX-XU@users.noreply.github.com>
2022-01-09 20:09:06 -0800

    Update ispalindrome.cpp

--- ispalindrome.cpp
+++ ispalindrome.cpp
@@ -3,11 +3,11 @@
 #include <vector>
 using namespace std;
 
-bool is_Palindrome(str test)
+bool is_Palindrome(int test)
 {
-    for(int i = 0; i <= str.length()-1; i++)
+    for(int i = 0; i <= test.length()-1; i++)
     {
-        if(str[i] != str[str.length()-1-i])
+        if(test[i] != testt[test.length()-1-i])
         {
             return false;
         }
